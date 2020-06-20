a6c453402f1b6d75d722b08e80c0a700be57ab98
merito <brigit87@gmail.com>
2020-06-20 16:14:29 +0530

    Updates

---
+++ precache-manifest.5a7db3224ca6284bb5cd1ef415d15b93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e18266dbece52bae8660da51c691140",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "6750f19483d777265edc",
+    "url": "/restaurant/static/css/2.11fbe80b.chunk.css"
+  },
+  {
+    "revision": "39c64e67cd5bbff3b624",
+    "url": "/restaurant/static/css/main.8c2836e9.chunk.css"
+  },
+  {
+    "revision": "6750f19483d777265edc",
+    "url": "/restaurant/static/js/2.126a3abf.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/restaurant/static/js/2.126a3abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c64e67cd5bbff3b624",
+    "url": "/restaurant/static/js/main.e245a6bf.chunk.js"
+  },
+  {
+    "revision": "807be019555a46e16540",
+    "url": "/restaurant/static/js/runtime-main.02b6ab67.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/restaurent/precache-manifest.8fe1ccf86a9cdd8dd966ffea92591ffd.js"
+  "/restaurant/precache-manifest.5a7db3224ca6284bb5cd1ef415d15b93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restaurent/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restaurant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
